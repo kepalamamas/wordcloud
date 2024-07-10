de0d60e71a72cccd8c1d71539eb8242fadc5cf98
Indrakusuma <kindrakusuma@deloitte.com>
2024-07-10 23:29:22 +0700

    curcollistall

--- src/pages/curcollistall.tsx
+++ src/pages/curcollistall.tsx
@@ -9,7 +9,7 @@ interface IParagonListProps {
 
 const ParagonList: React.FunctionComponent<IParagonListProps> = (props) => {
   const [data1, setData1] = useState<Paragon[]>([]);
-  // const [data2, setData2] = useState<Paragon[]>([]);
+  const [data2, setData2] = useState<Paragon[]>([]);
   // const [data3, setData3] = useState<Paragon[]>([]);
   const [isLoaded, setIsLoaded] = useState(Boolean);
   const router = useRouter();
@@ -18,42 +18,22 @@ const ParagonList: React.FunctionComponent<IParagonListProps> = (props) => {
     if (!isLoaded) {
       axios
         .get(`https://konseruntuk.online/api/data1?_sort=id&_order=desc`)
-        .then((res) => {
-          setData1(res.data);
-        })
+        .then((res) => setData1(res.data))
         .catch((err) => console.log(err));
-      // axios
-      //   .get(`https://konseruntuk.online/api/data2?_sort=id&_order=desc`)
-      //   .then((res) => {
-      //     setData2(res.data);
-      //   })
-      //   .catch((err) => console.log(err));
-      // axios
-      //   .get(`https://konseruntuk.online/api/data3?_sort=id&_order=desc`)
-      //   .then((res) => {
-      //     setData3(res.data);
-      //   })
-      //   .catch((err) => console.log(err));
+      axios
+        .get(`https://konseruntuk.online/api/data2?_sort=id&_order=desc`)
+        .then((res) => setData2(res.data))
+        .catch((err) => console.log(err));
       setIsLoaded(true);
       setInterval(() => {
         axios
           .get(`https://konseruntuk.online/api/data1?_sort=id&_order=desc`)
-          .then((res) => {
-            setData1(res.data);
-          })
+          .then((res) => setData1(res.data))
           .catch((err) => console.log(err));
-        // axios
-        //   .get(`https://konseruntuk.online/api/data2?_sort=id&_order=desc`)
-        //   .then((res) => {
-        //     setData2(res.data);
-        //   })
-        //   .catch((err) => console.log(err));
-        // axios
-        //   .get(`https://konseruntuk.online/api/data3?_sort=id&_order=desc`)
-        //   .then((res) => {
-        //     setData3(res.data);
-        //   })
-        //   .catch((err) => console.log(err));
+        axios
+          .get(`https://konseruntuk.online/api/data2?_sort=id&_order=desc`)
+          .then((res) => setData2(res.data))
+          .catch((err) => console.log(err));
       }, 10000);
     }
   }, []);
@@ -67,17 +47,39 @@ const ParagonList: React.FunctionComponent<IParagonListProps> = (props) => {
           .delete(`https://konseruntuk.online/api/data1/${data.id}`)
           .catch((err) => console.log(err))
       });
-      alert("berhasil delete data")
-      router.push("/curcollistall")
+      alert("berhasil delete data");
+    }
+  }
+
+  const handleDeleteAll2 = () => {
+    const deleteKataAll = window.confirm("Are you sure want to delete all?")
+
+    if (deleteKataAll) {
+      data2.forEach((data) => {
+        axios
+          .delete(`https://konseruntuk.online/api/data2/${data.id}`)
+          .catch((err) => console.log(err))
+      });
+      alert("berhasil delete data");
     }
   }
 
   const handleDelete = (id: any) => {
-    const deleteKataAll = window.confirm("Are you sure want to delete?")
+    const deleteKata = window.confirm("Are you sure want to delete?")
 
-    if (deleteKataAll) {
+    if (deleteKata) {
       axios
         .delete(`https://konseruntuk.online/api/data1/${id}`)
+        .catch((err) => console.log(err))
+    };
+  }
+
+  const handleDelete2 = (id: any) => {
+    const deleteKata = window.confirm("Are you sure want to delete?")
+
+    if (deleteKata) {
+      axios
+        .delete(`https://konseruntuk.online/api/data2/${id}`)
         .catch((err) => console.log(err))
     };
   }
@@ -86,10 +88,10 @@ const ParagonList: React.FunctionComponent<IParagonListProps> = (props) => {
     <>
       <div className='container'>
         <h1>All List</h1>
-        <button onClick={() => handleDeleteAll()}>Delete All</button>
         <div className='row'>
           <div className="list-holder">
             <h2>Data 1</h2>
+            <button onClick={() => handleDeleteAll()}>Delete All Data 1</button>
             {data1.map((data) => {
               return (
                 <div className='data-item' key={String(data.id)}>
@@ -105,32 +107,24 @@ const ParagonList: React.FunctionComponent<IParagonListProps> = (props) => {
               )
             })}
           </div>
-          {/* <div className="list-holder">
+          <div className="list-holder">
             <h2>Data 2</h2>
+            <button onClick={() => handleDeleteAll2()}>Delete All Data 2</button>
             {data2.map((data) => {
               return (
-                <div className='data-item' key={String(data.id)} onClick={() => {
-                  router.push({
-                    pathname: "/paragondetail2",
-                    query: { id: String(data.id) }
-                  })
-                }}>{data.kata}</div>
+                <div className='data-item' key={String(data.id)}>
+                  {data.kata}
+                  <button style={{ float: "right" }} onClick={() => handleDelete2(data.id)}>Delete</button>
+                  <button style={{ float: "right" }} onClick={() => {
+                    router.push({
+                      pathname: "/curcoldetail",
+                      query: { id: String(data.id) }
+                    })
+                  }}>Detail</button>
+                </div>
               )
             })}
-          </div> */}
-          {/* <div className="list-holder">
-            <h2>Data 3</h2>
-            {data3.map((data) => {
-              return (
-                <div className='data-item' key={String(data.id)} onClick={() => {
-                  router.push({
-                    pathname: "/paragondetail3",
-                    query: { id: String(data.id) }
-                  })
-                }}>{data.kata}</div>
-              )
-            })}
-          </div> */}
+          </div>
         </div>
       </div>
     </>
